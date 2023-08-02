e1b7a54977034f07091eef8fb45f17fc5dd5f3f4
LitavrinaAA <a.a.litavrina@google.com>
2023-08-02 16:49:06 +0300

    вторая задача выполнена

--- homeTask1Ex002/Program.cs
+++ homeTask1Ex002/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+Console.WriteLine("Введите три числа, а я найду самое большое.");
+Console.Write("Введите первое число: ");
+int firstNum = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondtNum = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int thirdNum = int.Parse(Console.ReadLine());
+int max = firstNum;
+if (secondtNum > max)
+    max = secondtNum;
+if (thirdNum > max)
+    max = thirdNum;
+
+ Console.WriteLine("Самое большое число:" + max);
+
