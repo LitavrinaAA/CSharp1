7ef5e73806e108ad4f009fa71b0a1b5b4c560f9d
LitavrinaAA <a.a.litavrina@google.com>
2023-08-02 16:29:13 +0300

    первая задача

--- HomeTask1/Program.cs
+++ HomeTask1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+Console.WriteLine("Я принимаю два числа и выдю, какое число большее, а какое меньшее.");
+Console.Write("Введите первое число: ");
+int firstNum = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondNum = int.Parse(Console.ReadLine());
+if (firstNum > secondNum){
+    Console.WriteLine("Самое большое первое число " + firstNum 
+                + ", а самое маленькое второе " + secondNum);
+}
+else {
+    Console.WriteLine("Самое большоес второе число " + secondNum 
+                + ", а самое маленькое первое " + firstNum);  
+}
